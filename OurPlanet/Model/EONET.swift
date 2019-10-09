f4e6121c62120be4ddf91b4f37fd7870619c79cb
Rui Cao <rayray199085@gmail.com>
2019-10-09 17:50:16 +1100

    Complete this chapter

--- OurPlanet/Model/EONET.swift
+++ OurPlanet/Model/EONET.swift
@@ -31,72 +31,89 @@ import RxSwift
 import RxCocoa
 
 class EONET {
-  static let API = "https://eonet.sci.gsfc.nasa.gov/api/v2.1"
-  static let categoriesEndpoint = "/categories"
-  static let eventsEndpoint = "/events"
-
-  static func jsonDecoder(contentIdentifier: String) -> JSONDecoder {
-    let decoder = JSONDecoder()
-    decoder.userInfo[.contentIdentifier] = contentIdentifier
-    decoder.dateDecodingStrategy = .iso8601
-    return decoder
-  }
-
-  static var ISODateReader: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.locale = Locale(identifier: "en_US_POSIX")
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
-    return formatter
-  }()
-  
-  static func filteredEvents(events: [EOEvent], forCategory category: EOCategory) -> [EOEvent] {
-    return events.filter { event in
-      return event.categories.contains(category.id) &&
-             !category.events.contains {
-               $0.id == event.id
-             }
+    
+    static let API = "https://eonet.sci.gsfc.nasa.gov/api/v2.1"
+    static let categoriesEndpoint = "/categories"
+    static let eventsEndpoint = "/events"
+    
+    static var ISODateReader: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
+        return formatter
+    }()
+    
+    static func filteredEvents(events: [EOEvent], forCategory category: EOCategory) -> [EOEvent] {
+        return events
+            .filter { event in
+                event.categories.contains(category.id) && !category.events.contains { $0.id == event.id }
+            }
+            .sorted(by: EOEvent.compareDates)
     }
-    .sorted(by: EOEvent.compareDates)
-  }
-
-  static func request(endpoint: String, query: [String: Any] = [:])-> Observable<[String: Any]>{
-    do {
-      guard let url = URL(string: API)?.appendingPathComponent(endpoint),
-        var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
-          throw EOError.invalidURL(endpoint)
-      }
-      components.queryItems = try query.compactMap { (key, value) in
-        guard let v = value as? CustomStringConvertible else {
-          throw EOError.invalidParameter(key, value)
+    
+    static func request(endpoint: String, query: [String: Any] = [:]) -> Observable<[String: Any]> {
+        do {
+            guard let url = URL(string: API)?.appendingPathComponent(endpoint),
+                var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+                    throw EOError.invalidURL(endpoint)
+            }
+            
+            components.queryItems = try query.compactMap {
+                guard let v = $1 as? CustomStringConvertible else {
+                    throw EOError.invalidParameter($0, $1)
+                }
+                return URLQueryItem(name: $0, value: v.description)
+            }
+            
+            guard let finalURL = components.url else {
+                throw EOError.invalidURL(endpoint)
+            }
+            
+            return URLSession.shared.rx.response(request: URLRequest(url: finalURL))
+                .map { _, d -> [String: Any] in
+                    guard let json = try? JSONSerialization.jsonObject(with: d, options: []),
+                        let r = json as? [String: Any] else {
+                            throw EOError.invalidJSON(finalURL.absoluteString)
+                    }
+                    return r
+            }
+        } catch {
+            return Observable.empty()
         }
-        return URLQueryItem(name: key, value: v.description)
-      }
-      guard let finalURL = components.url else {
-        throw EOError.invalidURL(endpoint)
-      }
-      let request = URLRequest(url: finalURL)
-
-      return URLSession.shared.rx.response(request: request)
-        .map { _, data -> [String: Any] in
-          guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
-            let result = jsonObject as? [String: Any] else {
-              throw EOError.invalidJSON(finalURL.absoluteString)
-          }
-          return result
-      }
-    } catch {
-      return Observable.empty()
     }
-  }
-  static var categories: Observable<[EOCategory]> = {
-    return EONET.request(endpoint: categoriesEndpoint)
-      .map { data in
-        let categories = data["categories"] as? [[String: Any]] ?? []
-        return categories
-          .compactMap(EOCategory.init)
-          .sorted { $0.name < $1.name }
-      }
-      .catchErrorJustReturn([])
-      .share(replay: 1, scope: .forever)
-  }()
+    
+    static let categories: Observable<[EOCategory]> = {
+        return EONET.request(endpoint: categoriesEndpoint)
+            .map {
+                guard let raw = $0["categories"] as? [[String: Any]] else {
+                    throw EOError.invalidJSON(categoriesEndpoint)
+                }
+                return raw
+                    .compactMap(EOCategory.init)
+                    .sorted { $0.name < $1.name }
+            }
+            .catchErrorJustReturn([])
+            .share(replay: 1, scope: .forever)
+    }()
+    
+    static func events(forLast days: Int = 360, category: EOCategory) -> Observable<[EOEvent]> {
+        let openEvents = events(forLast: days, closed: false, endpoint: category.endpoint)
+        let closedEvents = events(forLast: days, closed: true, endpoint: category.endpoint)
+        return Observable.of(openEvents, closedEvents)
+            .merge()
+            .reduce([]) { $0 + $1 }
+    }
+    
+    fileprivate static func events(forLast days: Int, closed: Bool, endpoint: String) -> Observable<[EOEvent]> {
+        return request(endpoint: endpoint,
+                       query: ["days": NSNumber(value: days), "status": (closed ? "closed" : "open")])
+            .map {
+                guard let raw = $0["events"] as? [[String: Any]] else {
+                    throw EOError.invalidJSON(endpoint)
+                }
+                return raw.compactMap(EOEvent.init)
+            }
+            .catchErrorJustReturn([])
+    }
+    
 }
